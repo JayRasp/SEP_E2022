226701679f213b285f107b242f832152233725a4
Joshua Weber <joshua.weber1996@gmail.com>
2021-03-19 12:05:38 +0100

    css adjustments

--- www/quizinfo.php
+++ www/quizinfo.php
@@ -58,7 +58,7 @@ if(!$error){
 </br>
 </br>
   <form class="alignRight" action="/testQuiz.php">
-    <input class="menuItem" type="submit" value="Test">
+    <input class="button" type="submit" value="Test">
   </form>
 </div>
         <footer>
