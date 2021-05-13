982898e8be58abfaa6f8b0bf5d6c17723040766c
Joshua Weber <joshua.weber1996@gmail.com>
2021-05-13 23:10:32 +0200

    PhpMyadmin button

--- www/quizinfo.php
+++ www/quizinfo.php
@@ -68,8 +68,26 @@ if(!$error){
 </br>
 <div class="alignCenter">
   <a class="button" href="quiz.php?category=all">Test</a>
+    <a class="button" href=":8080">phpMyAdmin</a>
 </div>
 </div>
+
+<script>
+//
+// delegate event for performance, and save attaching a million events to each anchor
+document.addEventListener('click', function(event) {
+  var target = event.target;
+  if (target.tagName.toLowerCase() == 'a')
+  {
+      var port = target.getAttribute('href').match(/^:(\d+)(.*)/);
+      if (port)
+      {
+         target.href = window.location.origin;
+         target.port = port[1];
+      }
+  }
+}, false);
+</script>
         <footer>
 
         </footer>
