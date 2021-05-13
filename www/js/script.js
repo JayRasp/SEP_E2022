491ef9447736f4d8cd2ea49f7a4082eebe431045
Joshua Weber <joshua.weber1996@gmail.com>
2021-05-13 22:55:40 +0200

    Shuffle choices(answers)

--- www/js/script.js
+++ www/js/script.js
@@ -61,6 +61,26 @@ class Question {
     choices: function() {
       let choices = quiz.getCurrentQuestion().choices;
 
+      function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+shuffle(choices);
+
       guessHandler = (id, guess) => {
         document.getElementById(id).onclick = function() {
           quiz.guess(guess);
