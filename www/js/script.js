45bc7c1ee0727f5651e02da23a5ae54814761aff
Joshua Weber <joshua.weber1996@gmail.com>
2021-05-14 12:10:19 +0200

    repositioned shuffle function

--- www/js/script.js
+++ www/js/script.js
@@ -9,7 +9,24 @@ class Question {
     }
   }
   //utf8_encode('string') needed to display 'à' , 'é', etc...
+  function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+  // Pick a remaining element...
+  randomIndex = Math.floor(Math.random() * currentIndex);
+  currentIndex -= 1;
+
+  // And swap it with the current element.
+  temporaryValue = array[currentIndex];
+  array[currentIndex] = array[randomIndex];
+  array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+  }
 
   //console.log(questions);
 
@@ -61,24 +78,7 @@ class Question {
     choices: function() {
       let choices = quiz.getCurrentQuestion().choices;
 
-      function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
 shuffle(choices);
 
       guessHandler = (id, guess) => {
